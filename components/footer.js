1516d7d06af6570ffc60dee311ee297dbb55cda6
Delta P <you@example.com>
2022-06-13 14:32:02 +0530

    footer

---
+++ components/footer.js
@@ -0,0 +1,72 @@
+let footer = () => {
+    return `<hr>
+    <div id="footer_main">
+        <div>
+            <h6 class="footer-h6">CUSTOMER CARE</h6>
+            <p class="footer-p">Shopper Protection</p>
+            <p class="footer-p">Loyalty Program</p>
+            <p class="footer-p">Help Center</p>
+            <p class="footer-p">Size Guides</p>
+            <p class="footer-p">Contact Us / Feedback</p>
+            <p class="footer-p"> Shipping / Returns</p>
+        </div>
+
+        <div>
+            <h6 class="footer-h6">INFORMATION</h6>
+            <p class="footer-p">About Us</p>
+            <p class="footer-p">Influencer Program</p>
+            <p class="footer-p">Partner Stores</p>
+            <p class="footer-p">Sitemap</p>
+        </div>
+
+        <div>
+            <h6 class="footer-h6">LEGAL</h6>
+            <p class="footer-p">Terms Of Use</p>
+            <p class="footer-p">Disclosure</p>
+            <p class="footer-p">Privacy Policy</p>
+            <p class="footer-p">Community Guidelines</p>
+
+        </div>
+
+        <div>
+            <h6 class="footer-h6">CONNECT WITH US</h6>
+            <p class="footer-p">ModeSens App for IOS and Android</p>
+            <p class="footer-p">Add to Browser</p>
+        </div>
+
+        <div>
+            <h6 class="footer-h6">NEWSLETTER SIGN UP</h6>
+            <div id="input">
+                <input type="email" placeholder="Email Address">
+            </div>
+            <dv id="news_p">
+                <input type="checkbox">
+                <h6>Women's</h6>
+                <input type="checkbox">
+                <h6>Men's</h6>
+            </dv>
+            
+                <button id="submit">SUBMIT</button>
+            
+        </div>
+    </div>
+
+    <hr>
+
+    <div id="lower_footer">
+        <div id="lower_footer_div1">
+            <img src="https://cdn.modesens.com/static/img/20180905footer_logo.svg" alt="logo">
+            <p class="footer_p">2022, ModeSens Inc</p>
+        </div>
+        <div id="lower_footer_div2">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/20181008footer-pit.svg" alt="">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/20181008footer-tt.svg" alt="">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/footer-p.svg" alt="">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/footer-fb.svg" alt="">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/footer-wb.svg" alt="">
+            <img class="footer_img" src="https://cdn.modesens.com/static/img/footer-wx.svg" alt="">
+        </div>
+    </div>`;
+};
+
+export default footer;
--- script/index.js
+++ script/index.js
@@ -3,3 +3,8 @@ import {navbar} from "../components/navbar.js";
 let navbar1 = document.getElementById("navbar");
 
 navbar1.innerHTML = navbar();
+
+import footer from "../components/footer.js";
+
+
+let footer1 = document.getElementById("footer").innerHTML = footer();
